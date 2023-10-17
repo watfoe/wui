608d007cb70e552779a17f4856915328d92ba245
Jimmie Lovell <jimmieomlovell@gmail.com>
2023-10-17 06:22:48 +0300

    fix: package entry point

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,9 +1,18 @@
 export * as buttons from './buttons';
+export * from './buttons';
 export * as display from './display';
+export * from './display';
 export * as domains from './domains';
+export * from './domains';
 export * as feedback from './feedback';
+export * from './feedback';
 export * as inputs from './inputs';
+export * from './inputs';
 export * as layout from './layout';
+export * from './layout';
 export * as navigation from './navigation';
+export * from './navigation';
 export * as typography from './typography';
+export * from './typography';
 export * as utils from './utils';
+export * from './utils';
