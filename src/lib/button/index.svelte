74e5848fc2330cccc37cf84842097cf62360e30d
Jimmie Lovell <jimmieomlovell@gmail.com>
2024-01-31 13:57:21 +0300

    fix: make neutral colors more legible

--- src/lib/button/index.svelte
+++ src/lib/button/index.svelte
@@ -1,95 +1,103 @@
 <script context="module" lang="ts">
-  import type { HTMLButtonAttributes } from 'svelte/elements';
+	import type { HTMLButtonAttributes } from 'svelte/elements';
 
-  export interface ButtonProps extends HTMLButtonAttributes {
-    anchorfor?: string;
-    bold?: boolean;
-    variant?: 'solid' | 'outline' | 'soft' | 'plain' | 'none';
-    color?: 'primary' | 'neutral' | 'success' | 'warning' | 'danger';
-    size?: 'sm' | 'md' | 'lg';
-    gap?: 'sm' | 'nm' | 'md' | 'lg';
-    justify?: 'flex-start' | 'flex-end' | 'center' | 'space-between' | 'space-around';
-    loading?: boolean;
-    prefix?: string;
-    suffix?: string;
-    width?: 'full' | 'half' | 'third' | 'quarter' | 'auto' | 'inherit';
-    height?: 'full' | 'half' | 'third' | 'quarter' | 'auto' | 'inherit';
-    element?: HTMLButtonElement;
-  }
+	export interface ButtonProps extends HTMLButtonAttributes {
+		anchorfor?: string;
+		bold?: boolean;
+		variant?: 'solid' | 'outline' | 'soft' | 'plain' | 'none';
+		color?: 'primary' | 'neutral' | 'success' | 'warning' | 'danger';
+		size?: 'sm' | 'md' | 'lg';
+		gap?: 'sm' | 'nm' | 'md' | 'lg';
+		justify?: 'flex-start' | 'flex-end' | 'center' | 'space-between' | 'space-around';
+		loading?: boolean;
+		prefix?: string;
+		suffix?: string;
+		width?: 'full' | 'half' | 'third' | 'quarter' | 'auto' | 'inherit';
+		height?: 'full' | 'half' | 'third' | 'quarter' | 'auto' | 'inherit';
+		element?: HTMLButtonElement;
+	}
 </script>
 
 <script lang="ts">
-  import './style.css';
-  import {Icon} from '$lib/display';
-  import {Text} from '$lib/typography';
+	import './style.css';
+	import { Icon } from '$lib/display';
+	import { Text } from '$lib/typography';
 	import { onMount } from 'svelte';
 
-  interface $$Props extends ButtonProps {}
+	interface $$Props extends ButtonProps {}
 
-  export let bold: $$Props['bold'] = true;
-  export let gap: $$Props['gap'] = 'nm';
-  export let size: $$Props['size'] = 'md';
-  export let variant: $$Props['variant'] = 'solid';
-  export let color: $$Props['color'] = 'primary';
-  export let loading: $$Props['loading'] = false;
-  export let justify: $$Props['justify'] = 'center';
-  export let disabled: $$Props['disabled'] = false;
-  export let anchorfor: $$Props['anchorfor'] = undefined;
-  export let prefix: $$Props['prefix'] = undefined;
-  export let suffix: $$Props['suffix'] = undefined;
-  export let width: $$Props['width'] = undefined;
-  export let height: $$Props['height'] = undefined;
-  export let element: $$Props['element'] = undefined;
+	export let bold: $$Props['bold'] = false;
+	export let gap: $$Props['gap'] = 'nm';
+	export let size: $$Props['size'] = 'md';
+	export let variant: $$Props['variant'] = 'solid';
+	export let color: $$Props['color'] = 'primary';
+	export let loading: $$Props['loading'] = false;
+	export let justify: $$Props['justify'] = 'center';
+	export let disabled: $$Props['disabled'] = false;
+	export let anchorfor: $$Props['anchorfor'] = undefined;
+	export let prefix: $$Props['prefix'] = undefined;
+	export let suffix: $$Props['suffix'] = undefined;
+	export let width: $$Props['width'] = undefined;
+	export let height: $$Props['height'] = undefined;
+	export let element: $$Props['element'] = undefined;
 
-  let feedback: HTMLDialogElement;
+	let feedback: HTMLDialogElement;
 
-  onMount(() => {
-    if (anchorfor) {
-      const _feedback = document.getElementById(anchorfor);
+	onMount(() => {
+		if (anchorfor) {
+			const _feedback = document.getElementById(anchorfor);
 
-      if (_feedback instanceof HTMLDialogElement) {
-        feedback = _feedback;
-      } else {
-        throw new Error(`button "anchorfor" attribute must be a valid dialog id`);
-      }
-    }
-  });
+			if (_feedback instanceof HTMLDialogElement) {
+				feedback = _feedback;
+			} else {
+				throw new Error(`button "anchorfor" attribute must be a valid dialog id`);
+			}
+		}
+	});
 
-  function click(e: CustomEvent<HTMLButtonElement>) {
-    feedback?.dispatchEvent(new CustomEvent('open', { detail: {
-      anchor: e.currentTarget,
-    }}));
-  };
+	function click(e: CustomEvent<HTMLButtonElement>) {
+		feedback?.dispatchEvent(
+			new CustomEvent('open', {
+				detail: {
+					anchor: e.currentTarget
+				}
+			})
+		);
+	}
 </script>
 
 <button
-  {...$$restProps}
-  bind:this={element}
-  class="WuiButton WuiButton--{variant} WuiButton--{size} WuiButton--{color} WuiButton--gap-{gap} {width ? 'WuiButton--width-' + width : ''} {height ? 'WuiButton--height-' + height : ''} {!$$slots.default ? 'WuiButton--only-icon' : ''} {$$restProps.class || ''}"
-  disabled="{loading || disabled}"
-  style="{$$restProps.style || ''};--WuiButtonFlex-justify:{justify};"
-  on:click={click}
-  on:*
+	{...$$restProps}
+	bind:this={element}
+	class="WuiButton WuiButton--{variant} WuiButton--{size} WuiButton--{color} WuiButton--gap-{gap} {width
+		? 'WuiButton--width-' + width
+		: ''} {height ? 'WuiButton--height-' + height : ''} {!$$slots.default
+		? 'WuiButton--only-icon'
+		: ''} {$$restProps.class || ''}"
+	disabled={loading || disabled}
+	style="{$$restProps.style || ''};--WuiButtonFlex-justify:{justify};"
+	on:click={click}
+	on:*
 >
-  {#if loading}
-    <span class="WuiButton__loader" />
-  {:else}
-    {#if $$slots.prefix}
-      <slot name="prefix" />
-    {:else if prefix}
-      <Icon size={size}>{prefix}</Icon>
-    {/if}
+	{#if loading}
+		<span class="WuiButton__loader" />
+	{:else}
+		{#if $$slots.prefix}
+			<slot name="prefix" />
+		{:else if prefix}
+			<Icon {size}>{prefix}</Icon>
+		{/if}
 
-    {#if $$slots.default}
-      <Text bold={bold} size="md" class="WuiButton__text">
-        <slot />
-      </Text>
-    {/if}
+		{#if $$slots.default}
+			<Text {bold} size="md" class="WuiButton__text">
+				<slot />
+			</Text>
+		{/if}
 
-    {#if $$slots.suffix}
-      <slot name="suffix" />
-    {:else if suffix}
-      <Icon size={size}>{suffix}</Icon>
-    {/if}
-  {/if}
+		{#if $$slots.suffix}
+			<slot name="suffix" />
+		{:else if suffix}
+			<Icon {size}>{suffix}</Icon>
+		{/if}
+	{/if}
 </button>
--- src/lib/inputs/label/index.svelte
+++ src/lib/inputs/label/index.svelte
@@ -1,43 +1,30 @@
 <script lang="ts">
-  import { Col } from "$lib/layout";
-  import { Text } from "$lib/typography";
+	import './style.css';
+	import { Col } from '$lib/layout';
+	import { Text } from '$lib/typography';
 	import type { HTMLLabelAttributes } from 'svelte/elements';
 
-  interface $$Props extends HTMLLabelAttributes {
-    description?: string;
-  }
+	interface $$Props extends HTMLLabelAttributes {
+		description?: string;
+	}
 
-  export let description: $$Props['description'] = undefined;
+	export let description: $$Props['description'] = undefined;
 </script>
 
 <Col justify="flex-start" align="flex-start" class="WuiInput__label__root">
-  <Text variant="label" size="md" bold class="WuiInput__label" {...$$restProps}>
-    {#if $$slots.default}
-      <slot />
-    {/if}
-  </Text>
+	<Text variant="label" size="md" bold class="WuiInput__label" {...$$restProps}>
+		{#if $$slots.default}
+			<slot />
+		{/if}
+	</Text>
 
-  {#if $$slots.description}
-    <Text variant="label" size="sm" color="neutral" class="WuiInput__label__desc">
-      <slot name="description" />
-    </Text>
-  {:else if description}
-    <Text variant="label" size="sm" color="neutral" class="WuiInput__label__desc">{description}</Text>
-  {/if}
+	{#if $$slots.description}
+		<Text variant="label" size="sm" color="neutral" class="WuiInput__label__desc">
+			<slot name="description" />
+		</Text>
+	{:else if description}
+		<Text variant="label" size="sm" color="neutral" class="WuiInput__label__desc"
+			>{description}</Text
+		>
+	{/if}
 </Col>
-
-<style>
-  :global(.WuiInput__label__root) {
-    width: 100% !important;
-    max-width: calc(100% - (2*var(--space-nm)));
-    margin-left: var(--space-nm);
-  }
-  :global(.WuiInput__label) {
-    color: var(--WuiLabel-color, var(--color-black-1)) !important;
-    margin-bottom: 1px;
-  }
-
-  :global(.WuiInput__label__desc) {
-    margin-bottom: 3px;
-  }
-</style>
