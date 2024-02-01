ff13c8e6bfdd202ae5e91206f76f971f58253319
Jimmie Lovell <jimmieomlovell@gmail.com>
2024-02-02 00:43:53 +0300

    feat: add size, color and orientation to divider component

--- src/lib/display/divider/index.svelte
+++ src/lib/display/divider/index.svelte
@@ -1,10 +1,68 @@
-<div role="separator"></div>
+<script lang="ts">
+	interface $$Props {
+		color?: 'primary' | 'neutral' | 'success' | 'warning' | 'danger';
+		size?: 'sm' | 'md' | 'lg';
+		vertical?: boolean;
+	}
+
+	export let color: $$Props['color'] = 'neutral';
+	export let size: $$Props['size'] = 'sm';
+	export let vertical: $$Props['vertical'] = false;
+</script>
+
+<div
+	role="separator"
+	class="WuiDivider WuiDivider--{size} WuiDivider--{color} {vertical
+		? 'WuiDivider--vert'
+		: 'WuiDivider--horiz'}"
+/>
 
 <style>
-  div {
-    background-color: var(--color-outline);
-    width: 100%;
-    height: 0.9px;
-    margin: var(--space-xs) 0;
-  }
+	.WuiDivider {
+		--WuiDivider--color: var(--color-outline);
+		--WuiDivider--size: 1px;
+		--WuiDivider--margin: var(--space-xs);
+		background-color: var(--WuiDivider--color);
+	}
+
+	.WuiDivider--horiz {
+		width: 100%;
+		height: var(--WuiDivider--size);
+		margin: var(--WuiDivider--margin) 0;
+	}
+
+	.WuiDivider--vert {
+		width: var(--WuiDivider--size);
+		height: 100%;
+		margin: 0 var(--WuiDivider--margin);
+	}
+
+	.WuiDivider--sm {
+		--WuiDivider--size: 1px;
+		--WuiDivider--margin: var(--space-xs);
+	}
+	.WuiDivider--md {
+		--WuiDivider--size: 2px;
+		--WuiDivider--margin: var(--space-sm);
+	}
+	.WuiDivider--lg {
+		--WuiDivider--size: 3px;
+		--WuiDivider--margin: var(--space-nm);
+	}
+
+	.WuiDivider--primary {
+		--WuiDivider--color: var(--color-primary);
+	}
+	.WuiDivider--neutral {
+		--WuiDivider--color: var(--color-outline);
+	}
+	.WuiDivider--success {
+		--WuiDivider--color: var(--color-success);
+	}
+	.WuiDivider--warning {
+		--WuiDivider--color: var(--color-warning);
+	}
+	.WuiDivider--danger {
+		--WuiDivider--color: var(--color-danger);
+	}
 </style>
